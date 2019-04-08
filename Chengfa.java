dedf9d339817675fadcce8cf4aff9414806745e0
liujunkaizzz <49388472+liujunkaizzz@users.noreply.github.com>
2019-04-08 15:08:24 +0800

    Add files via upload

---
+++ Chengfa.java
@@ -0,0 +1,21 @@
+package ljk;
+/**
+ * 输入数字.阶乘
+ */
+
+import java.util.Scanner;
+
+public class Chengfa {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int height=sc.nextInt();
+        for (int i = 1; i <= height; i++) {
+	        for (int j = 1; j <= i; j++) {
+				System.out.print(j+"*"+i+"="+(i*j)+" ");
+			}
+	        System.out.println();
+      }
+	}
+
+}
